ea2d0ee5ee7e4ac16270bcb44161048c573af95a
anitha259 <74256207+anitha259@users.noreply.github.com>
2021-08-06 21:23:30 +0530

    Add files via upload

---
+++ dom/dom.js
@@ -0,0 +1,43 @@
+// let product = [{
+//         name: "jaden",
+//         city: "newyork",
+
+//     },
+
+//     {
+//         name: "harish",
+//         city: "delhi"
+
+//     }
+// ]
+// console.log(product);
+
+
+// console.log(document.querySelector('.first'));
+
+// console.log(document.querySelector('.second'));
+
+// console.log(document.querySelector('#one'));
+
+
+
+function dosomething() {
+
+
+    console.log("jaden");
+    console.log("newyork")
+
+    console.log(document.querySelector("#inp").value);
+
+    // document.querySelector("#sec").innerHTML = `<h1>${name}</h1>`;
+
+
+    let name = document.querySelector("#inp").value;
+    document.querySelector("#sec").innerHTML = `<h1>${name}</h1>`;
+
+
+
+
+    // document.querySelector("#sec").innerHTML = "<h1>jaden</h1>";
+    // document.querySelector("#sec").innerHTML = "<h2>newyork</h2>";
+}
